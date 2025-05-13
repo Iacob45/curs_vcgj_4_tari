582b8db84321afab90523f4e8093a76f7563a320
elenadaniela16 <danuta160402@gmail.com>
2025-05-13 14:16:21 +0300

    modificare tari.py

--- tari.py
+++ tari.py
@@ -1,31 +1,22 @@
-from flask import Flask, url_for
-from app.lib import biblioteca_header, biblioteca_romania, biblioteca_germania, biblioteca_olanda, biblioteca_franta
+from flask import Flask
+from app.lib import biblioteca_header, biblioteca_olanda
 
 api = Flask(__name__)
 
 @api.route("/", methods=['GET'])
 def index() -> str:
-    text = biblioteca_header.header_descriere()
-    text += biblioteca_romania.descriere_romania()
-    text += biblioteca_germania.descriere_germania()
-    text += biblioteca_olanda.descriere_olanda()
-    text += biblioteca_franta.descriere_franta()
-    return text
+	text = biblioteca_header.header_descriere_olanda()
+	text += biblioteca_olanda.descriere_olanda()
 
 @api.route("/capitala", methods=['GET'])
 def capitala() -> str:
-    text = biblioteca_header.header_capitala()
-    text += biblioteca_romania.capitala_romania()
-    text += biblioteca_germania.capitala_germania()
-    text += biblioteca_olanda.capitala_olanda()
-    text += biblioteca_franta.capitala_franta()
+    
+    text = biblioteca_header.header_capitala_olanda()  
+    text += biblioteca_olanda.capitala_olanda()  
     return text
 
 @api.route("/steag", methods=['GET'])
 def steag() -> str:
-    text = biblioteca_header.header_steag()
-    text += biblioteca_romania.steag_romania()
-    text += biblioteca_germania.steag_germania()
-    text += biblioteca_olanda.steag_olanda()
-    text += biblioteca_franta.steag_franta()
+    text = biblioteca_header.header_steag_olanda()  
+    text += biblioteca_olanda.steag_olanda()  
     return text
