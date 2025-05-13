521e3ab548662cccde683ecb280d668a7db0d345
MantaValentina <valentina.manta@stud.etti.upb.ro>
2025-05-13 23:38:56 +0300

    modif_pylint

--- tari.py
+++ tari.py
@@ -1,22 +1,26 @@
 import sys
-from flask import Flask, url_for
+import pytest
+from flask import Flask
 from app.lib.biblioteca_canada import descriere_canada, capitala_canada, steag_canada
 
-
+""" Modulul `tari.py` conține funcții care generează pagini HTML pentru diverse țări (descriere, capitală, steag). """
 api = Flask(__name__)
 
 
 @api.route("/canada", methods=['GET'])
 def index() -> str:
+    """Returnează HTML-ul cu descrierea Canadei."""
     return descriere_canada()
 
 @api.route("/canada/capitala", methods=['GET'])
 def capitala() -> str:
+    """Returnează HTML-ul capitala Canadei."""
     return capitala_canada()
 
 
 @api.route("/canada/steag", methods=['GET'])
 def steag() -> str:
+    """Returnează HTML-ul cu drapelul Canadei."""
     return steag_canada()
 
 @api.cli.command()
@@ -27,10 +31,4 @@ def test():
     Apelare pytest din aplicatia systest, cu ajutorul comenzii flask.
 
     """
-    import pytest
     sys.exit(pytest.main(["."]))
-
-
-
-
-
