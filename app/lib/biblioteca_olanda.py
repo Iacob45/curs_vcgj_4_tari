f3e2d556b20c40a79478193ea32f99cc975e2b55
elenadaniela16 <danuta160402@gmail.com>
2025-05-13 13:17:49 +0300

    commit

--- app/lib/biblioteca_olanda.py
+++ app/lib/biblioteca_olanda.py
@@ -1,9 +1,6 @@
-
 def descriere_olanda() -> str:
-    return "<h3>Olanda</h3><br>Olanda este o tara<br><br><br>"
+	return "Olanda, cunoscută oficial ca Regatul Țărilor de Jos, este o țară din Europa de Vest, renumită pentru lalele, morile de vânt și canalele sale. Capitala, Amsterdam, este un important centru cultural, iar Rotterdam găzduiește unul dintre cele mai mari porturi din Europa. Cu o economie puternică bazată pe comerț, agricultură și tehnologie, Olanda este un lider în sustenabilitate și inovație. Cultura sa este influențată de mari artiști precum Rembrandt și Van Gogh. Turismul prosperă datorită orașelor pitorești, muzeelor și tradițiilor autentice."
+def capitala_olanda() ->str:
+	return "Amsterdam"
 
-def capitala_olanda() -> str:
-    return "<h3>Olanda</h3><br>Amsterdam<br><br><br>"
 
-def steag_olanda() -> str:
-    return "<h3>Olanda</h3><br>Acesta este un steag<br><br><br>"
