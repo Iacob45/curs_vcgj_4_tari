594c3b5036cb6ad2f6a93789a655971dae46e1de
Andreea Barbu <barbuandreea01@yahoo.com>
2025-05-16 18:15:07 +0100

    commit

--- tari.py
+++ tari.py
@@ -20,6 +20,15 @@ def steag() -> str:
     return biblioteca_brazilia.steag_brazilia()
 
 
+@api.cli.command()
+def test():
+    """
+    Rulare 'unit tests'
 
+    Apelare pytest din aplicatia systest, cu ajutorul comenzii flask.
+
+    """
+    import pytest
+    sys.exit(pytest.main(["."]))
 
 
