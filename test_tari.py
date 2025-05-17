58f659b8536484d9abfeb71a11a1c1eeead3fb87
CosminelBoss <cosminqazxc2000@gmail.com>
2025-05-17 13:59:45 +0300

    Adaugat tari.py si testele pentru Norvegia

---
+++ test_tari.py
@@ -0,0 +1,10 @@
+from app import descriere, capitala, steag
+
+def test_descriere():
+    assert "Norvegia" in descriere()
+
+def test_capitala():
+    assert "Oslo" in capitala()
+
+def test_steag():
+    assert "<img" in steag()
