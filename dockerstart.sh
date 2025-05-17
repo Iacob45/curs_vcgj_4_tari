08d85973dbfb8fab66c43762bb6a517a2d2e847f
andreea0128 <barbuandreea01@yahoo.com>
2025-05-17 18:57:39 +0100

    Modificare fisiere

--- dockerstart.sh
+++ dockerstart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "Activare venv:"
+echo "Activarea venv:"
 #source .vanv/bin/activate
 . .venv/bin/activate
 echo "Configurare variabila mediu FLASK_APP"
@@ -10,3 +10,4 @@ lsdir=`ls -1`
 echo "CONTINUT DIRECTOR: " $lsdir
 echo "Start server:"
 exec flask run -h 0.0.0.0 -p 5011 --reload
+
