56ab03982a15180c5b9a9c25516eb7f448c2a41b
CosminelBoss <cosminqazxc2000@gmail.com>
2025-05-18 15:09:38 +0300

    Am pastrat doar biblioteca Norvegia si am sters restul

--- app/lib/biblioteca_germania.py
+++
@@ -1,9 +0,0 @@
-
-def descriere_germania() -> str:
-    return "<h3>Germania</h3><br>Germania este o tara<br><br><br>"
-
-def capitala_germania() -> str:
-    return "<h3>Germania</h3><br>Berlin<br><br><br>"
-
-def steag_germania() -> str:
-    return "<h3>Germania</h3><br>Acesta este un steag<br><br><br>"
