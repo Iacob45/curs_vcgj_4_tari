3065c053222acab9eb795d2e12559fc61305718d
bugacatalin <catalinbuga2014@gmail.com>
2025-05-19 13:00:21 +0000

    Modificare pytest

--- app/lib/biblioteca_franta.py
+++
@@ -1,9 +0,0 @@
-
-def descriere_franta() -> str:
-    return "<h3>Franta</h3><br>Franta este o tara<br><br><br>"
-
-def capitala_franta() -> str:
-    return "<h3>Franta</h3><br>Paris<br><br><br>"
-
-def steag_franta() -> str:
-    return "<h3>Franta</h3><br>Acesta este un steag<br><br><br>"
